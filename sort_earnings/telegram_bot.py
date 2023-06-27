7f466405845236c147acf9e907f9fc6f49b28170
seung bin <ksbdaniel@naver.com>
2023-06-27 11:23:48 +0900

    refactor codes

--- sort_earnings/telegram_bot.py
+++ sort_earnings/telegram_bot.py
@@ -13,21 +13,24 @@ bot = telegram.Bot(api_token)
 updater = Updater(api_token, use_context=True)
 dispatcher = updater.dispatcher
 
-async def send_message():
-    bot.sendMessage(chat_id=chat_id, text=f"금주 실적발표 기업들입니다.")
+def send_message():
+    bot.sendMessage(chat_id=chat_id, text="3일 내 실적발표 기업들입니다.")
     for row in result.sort_close_earnings():
-        await bot.sendMessage(
+        bot.sendMessage(
             chat_id=chat_id, 
-            text=f"Ticker(종목코드): {row[0]} 종목명: {row[1]} 실적발표: {row[2]}")
+            text=f"Ticker(종목코드): {row[0]} 종목명: {row[1]} 실적발표: {row[2]}"
+            )
+    bot.sendMessage(chat_id=chat_id, text="집중관찰하고 싶은 종목코드를 ,로 구분하여 작성해주세요.")
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
     
 # asyncio.run(send_message())
 
 def echo(update, context):
     user_text = update.message.text
-    context.bot.send_message(chat_id=chat_id, text=user_text)
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-
-dispatcher.add_handler(echo_handler)
-
-updater.start_polling()
+    user_management = user_text.split(',')
+    print(user_management)
+    for row in user_management:
+        print(row)
