308e93183528236aa4c9e01f9cf153884010fdb1
seung bin <ksbdaniel@naver.com>
2023-07-01 18:22:11 +0900

    refactor codes by using mypy & pylint

--- sort_earnings/manufact_data.py
+++ sort_earnings/manufact_data.py
@@ -1,26 +1,38 @@
-import requests
+"""
+This module provides functions for sorting earnings calendar data.
+"""
+
 import csv
-import config as config
 import datetime
+from typing import List
+import requests
+import config
 
 
-api_key = config.ALPHAVANTAGE_API_KEY
-EARNINGS_CALENDAR_URL = 'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey={}'.format(api_key)
+api_key: str = config.ALPHAVANTAGE_API_KEY
+EARNINGS_CALENDAR_URL: str = f'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey={api_key}'
 
-def get_earnings_calendar():
+
+def get_earnings_calendar() -> List[List[str]]:
+    """
+    This function import earnings calendar to list
+    """
     with open('earnings_calendar.csv', 'w', encoding = "utf-8", newline='') as to_write:
         session = requests.Session()
         earnings_calendar = session.get(EARNINGS_CALENDAR_URL)
-        decoded_content = earnings_calendar.content.decode('utf-8')
-        my_list = list(csv.reader(decoded_content.splitlines(), delimiter=','))
+        decoded_content: str = earnings_calendar.content.decode('utf-8')
+        my_list: List[List[str]] = list(csv.reader(decoded_content.splitlines(), delimiter=','))
 
         writer = csv.writer(to_write)
         writer.writerows(my_list)
 
     return my_list
 
-def sort_close_earnings():
-    sort_result = []
+def sort_close_earnings() -> List[List[str]]:
+    """
+    This function sorts earning calendar to list
+    """
+    sort_result: List[List[str]] = []
     current = datetime.date.today()
 
     with open('sort_earnings.csv', 'w', encoding = "utf-8", newline='') as to_write:
@@ -30,10 +42,21 @@ def sort_close_earnings():
             if (announce_date - current).days < 3 and row[4] != '':
                 sort_result.append(row)
                 writer.writerow(row)
-                print(row)
 
     return sort_result
 
 sort_close_earnings()
 
 
+# sort_close_earnings()
+
+# totalrevenue 데이터 긁어와서 sort_earnings에 추가하는 로직 만들기
+# url에 2개의 변수가 있으면 어떻게 호출해야하는지?
+# 어떤 함수에서 호출해서 사용해야할지? api_key를 함수 바깥에 두면 나중에 다른 곳에서 호출할 때 지장이 없을지?
+# api_key = config.alphavantage_api_key
+# url = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=WBA&apikey={}".format(api_key)
+# r = requests.get(url)
+# data = r.json()
+# for row in data["quarterlyReports"]:
+#     ebitda = row["totalRevenue"]
+#     print(ebitda)
--- sort_earnings/telegram_bot.py
+++ sort_earnings/telegram_bot.py
@@ -1,3 +1,5 @@
+'''telegram bot'''
+from typing import List
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 
@@ -5,35 +7,40 @@ import config
 import manufact_data
 
 
-api_token = config.TELEGRAM_BOT_API
-chat_id = config.TELEGRAM_BOT_CHAT_API
+api_token: str = config.TELEGRAM_BOT_API
+chat_id: str = config.TELEGRAM_BOT_CHAT_API
 
 bot = telegram.Bot(api_token)
-updater = Updater(api_token, use_context=True)
-dispatcher = updater.dispatcher
+updater = Updater(token=api_token)
+dispatcher: telegram.ext.dispatcher.Dispatcher = updater.dispatcher
 
-def send_message():
+
+def send_message() -> None:
+    """
+    This function send messages by telegram bot
+    """
     bot.sendMessage(chat_id=chat_id, text="3일 내 실적발표 기업들입니다.")
     for row in manufact_data.sort_close_earnings():
         bot.sendMessage(
-            chat_id=chat_id, 
+            chat_id=chat_id,
             text=f"Ticker(종목코드): {row[0]} 종목명: {row[1]} 실적발표: {row[2]}"
             )
     bot.sendMessage(chat_id=chat_id, text="집중관찰하고 싶은 종목코드를 ,로 구분하여 작성해주세요.")
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    echo_handler: telegram.ext.messagehandler.MessageHandler = MessageHandler(Filters.text & ~Filters.command, echo)
     dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
-    
 # asyncio.run(send_message())
 
-def echo(update, context):
-    user_text = update.message.text.split(',')
-    user_management = [element.replace(' ','') for element in user_text]
+def echo(update: telegram.Update) -> List[str]:
+    """
+    I also don't know what is this function about
+    """
+    user_text: List[str] = update.message.text.split(',')
+    user_management: List[str] = [element.replace(' ','').upper() for element in user_text]
     print(user_management)
     for row in user_management:
         print(row)
-    
     return user_management
 
 send_message()
