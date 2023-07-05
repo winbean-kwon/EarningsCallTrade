e99de7c75b2cfabd67ef22c7b3f87a635035ed76
seung bin <ksbdaniel@naver.com>
2023-07-05 15:26:37 +0900

    modify function & test for getting total revenue

--- sort_earnings/manufact_data.py
+++ sort_earnings/manufact_data.py
@@ -8,17 +8,17 @@ from typing import List
 import requests
 import config
 
+import telegram_bot
 
 api_key: str = config.ALPHAVANTAGE_API_KEY
 EARNINGS_CALENDAR_URL: str = f'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey={api_key}'
-
+session = requests.Session()
 
 def get_earnings_calendar() -> List[List[str]]:
     """
-    This function import earnings calendar to list
+    import earnings calendar to list
     """
     with open('earnings_calendar.csv', 'w', encoding = "utf-8", newline='') as to_write:
-        session = requests.Session()
         earnings_calendar = session.get(EARNINGS_CALENDAR_URL)
         decoded_content: str = earnings_calendar.content.decode('utf-8')
         my_list: List[List[str]] = list(csv.reader(decoded_content.splitlines(), delimiter=','))
@@ -30,7 +30,7 @@ def get_earnings_calendar() -> List[List[str]]:
 
 def sort_close_earnings() -> List[List[str]]:
     """
-    This function sorts earning calendar to list
+    sorts earning calendar to list
     """
     sort_result: List[List[str]] = []
     current = datetime.date.today()
@@ -45,14 +45,48 @@ def sort_close_earnings() -> List[List[str]]:
 
     return sort_result
 
-sort_close_earnings()
+def append_input_totalrevenue() -> List[str]:
+    """
+    append totalrevenue from user input & close earnings
+    """
 
+    with open('sort_earnings.csv', 'r', encoding="utf-8") as to_read:
+        reader = csv.reader(to_read)
+        rows = list(reader)
+
+    for row in rows:
+        symbol = row[0]
+        income_statement: str = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={api_key}'
+        total_revenue = session.get(income_statement)
+        row.append(total_revenue)
+
+    with open('sort_earnings.csv','w', encoding = "utf-8", newline='') as to_write:
+        writer = csv.writer(to_write)
+        writer.writerows(rows)
 
 # sort_close_earnings()
 
-# totalrevenue 데이터 긁어와서 sort_earnings에 추가하는 로직 만들기
-# url에 2개의 변수가 있으면 어떻게 호출해야하는지?
-# 어떤 함수에서 호출해서 사용해야할지? api_key를 함수 바깥에 두면 나중에 다른 곳에서 호출할 때 지장이 없을지?
+with open('sort_earnings.csv', 'r', encoding="utf-8") as to_read:
+    reader = csv.reader(to_read)
+    rows = list(reader)
+
+for row in rows:
+    symbol = row[0]
+    income_statement: str = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={api_key}'
+    total_revenue = session.get(income_statement)
+    decoded_content: str = total_revenue.content.decode('utf-8')
+    row.append(decoded_content)
+
+    print(row)
+
+# with open('sort_earnings.csv','w', encoding = "utf-8", newline='') as to_write:
+#     writer = csv.writer(to_write)
+#     writer.writerows(rows)
+
+
+
+
+
 # api_key = config.alphavantage_api_key
 # url = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=WBA&apikey={}".format(api_key)
 # r = requests.get(url)
--- sort_earnings/telegram_bot.py
+++ sort_earnings/telegram_bot.py
@@ -1,5 +1,6 @@
 '''telegram bot'''
 from typing import List
+import csv
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 
@@ -14,10 +15,11 @@ bot = telegram.Bot(api_token)
 updater = Updater(token=api_token)
 dispatcher: telegram.ext.dispatcher.Dispatcher = updater.dispatcher
 
+updater.start_polling()
 
 def send_message() -> None:
     """
-    This function send messages by telegram bot
+    send messages by telegram bot
     """
     bot.sendMessage(chat_id=chat_id, text="3일 내 실적발표 기업들입니다.")
     for row in manufact_data.sort_close_earnings():
@@ -26,21 +28,22 @@ def send_message() -> None:
             text=f"Ticker(종목코드): {row[0]} 종목명: {row[1]} 실적발표: {row[2]}"
             )
     bot.sendMessage(chat_id=chat_id, text="집중관찰하고 싶은 종목코드를 ,로 구분하여 작성해주세요.")
-    echo_handler: telegram.ext.messagehandler.MessageHandler = MessageHandler(Filters.text & ~Filters.command, echo)
-    dispatcher.add_handler(echo_handler)
-
-    updater.start_polling()
+    manufact_data.append_input_totalrevenue()
 # asyncio.run(send_message())
 
-def echo(update: telegram.Update) -> List[str]:
+def echo(update: telegram.Update=None, context=None) -> List[str]:
     """
-    I also don't know what is this function about
+    user input at telegram bot
+    다른 곳에서 호출할 때 telegram bot에서 필요한 update와 context 인자에 어떤 값을 써야할지 몰라서 기본값을 None으로 설정
     """
     user_text: List[str] = update.message.text.split(',')
-    user_management: List[str] = [element.replace(' ','').upper() for element in user_text]
+    user_management = [element.replace(' ','').upper() for element in user_text]
     print(user_management)
-    for row in user_management:
-        print(row)
+
     return user_management
 
+
 send_message()
+
+echo_handler: telegram.ext.messagehandler.MessageHandler = MessageHandler(Filters.text & ~Filters.command, echo)
+dispatcher.add_handler(echo_handler)
