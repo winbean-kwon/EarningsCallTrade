7b5a17b1bb03d81bbbe18412092c59b34584343f
seungbin <ksbdaniel@naver.com>
2023-08-04 22:03:42 +0900

    crawle earning time and symbol to dic

--- trading/crawling.py
+++ trading/crawling.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -11,29 +10,30 @@ chrome_options.add_argument('window-size=1920,1080')
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=chrome_options)
 driver.implicitly_wait(5)
 
-
-
 driver.get('https://www.earningswhispers.com/calendar?sb=p&d=9&t=all')
 
 driver.implicitly_wait(5)
 
-
 cookie_check = driver.find_element(By.CLASS_NAME, "accept-policy").click()
-specific_time = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.caltime')
+specific_time_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.caltime')
+earning_company_name_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.calticker')
 earning_confirmed = driver.find_elements(By.CLASS_NAME, "epsdateconfirmed")
 earning_not_confirmed = driver.find_elements(By.CLASS_NAME, "epsdatenotconfirmed")
 earning_companies = earning_confirmed + earning_not_confirmed
 
-for i in range(1, len(earning_companies)*2):
-    driver.implicitly_wait(10)
-    if specific_time[i].text != '':
-        print(specific_time[i].text)
+specific_time = []
+earning_company_name = []
 
+#선택자중에 빈 문자열을 반환하는게 있어서 2배로 함
+for i in range(0, len(earning_companies)*2):
+    if specific_time_crawle[i].text != '':
+        specific_time.append(specific_time_crawle[i].text)
+
+# 빈 문자열을 포함하는 것들이 있어서 둘이 합치면 엉키게 됨
+for i in range(0, len(earning_companies)):
+    if earning_company_name_crawle[i].text != '':
+        earning_company_name.append(earning_company_name_crawle[i].text)
+
+total_crawle = {key: value for key, value in zip(earning_company_name,specific_time)}
+        
 driver.close()
-
-
-# /html/body/form/div[3]/section/ul/li[2]/div[1]/div[4]
-# //*[@id="T-BAC"]/div[4]
-
-# /html/body/form/div[3]/section/ul/li[3]/div[1]/div[4]
-# //*[@id="T-SCHW"]/div[4]
