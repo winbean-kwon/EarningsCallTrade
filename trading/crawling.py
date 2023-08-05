a3704d980fa0dd8e05a79a75f05e7d50ccb8a1c5
seungbin <ksbdaniel@naver.com>
2023-08-05 16:40:28 +0900

    modify crawling -> add after earnings

--- trading/crawling.py
+++ trading/crawling.py
@@ -14,26 +14,45 @@ driver.get('https://www.earningswhispers.com/calendar?sb=p&d=9&t=all')
 
 driver.implicitly_wait(5)
 
-cookie_check = driver.find_element(By.CLASS_NAME, "accept-policy").click()
-specific_time_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.caltime')
-earning_company_name_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.calticker')
-earning_confirmed = driver.find_elements(By.CLASS_NAME, "epsdateconfirmed")
-earning_not_confirmed = driver.find_elements(By.CLASS_NAME, "epsdatenotconfirmed")
-earning_companies = earning_confirmed + earning_not_confirmed
-
 specific_time = []
 earning_company_name = []
 
-#선택자중에 빈 문자열을 반환하는게 있어서 2배로 함
-for i in range(0, len(earning_companies)*2):
-    if specific_time_crawle[i].text != '':
-        specific_time.append(specific_time_crawle[i].text)
+def crawle_earning_data() :
+    specific_time.clear()
+    earning_company_name.clear()
 
-# 빈 문자열을 포함하는 것들이 있어서 둘이 합치면 엉키게 됨
-for i in range(0, len(earning_companies)):
-    if earning_company_name_crawle[i].text != '':
-        earning_company_name.append(earning_company_name_crawle[i].text)
+    elements = driver.find_elements(By.CLASS_NAME, "accept-policy")
 
-total_crawle = {key: value for key, value in zip(earning_company_name,specific_time)}
+    if elements:
+        driver.find_element(By.CLASS_NAME, "accept-policy").click()
+    specific_time_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.caltime')
+    earning_company_name_crawle = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.calticker')
+    earning_confirmed = driver.find_elements(By.CLASS_NAME, "epsdateconfirmed")
+    earning_not_confirmed = driver.find_elements(By.CLASS_NAME, "epsdatenotconfirmed")
+    earning_companies = earning_confirmed + earning_not_confirmed
+        
+
+    #선택자중에 빈 문자열을 반환하는게 있어서 2배로 함
+    for i in range(0, len(earning_companies)*2):
+        if specific_time_crawle[i].text != '':
+            specific_time.append(specific_time_crawle[i].text)
+
+    # 빈 문자열을 포함하는 것들이 있어서 둘이 합치면 엉키게 됨
+    for i in range(0, len(earning_companies)):
+        if earning_company_name_crawle[i].text != '':
+            earning_company_name.append(earning_company_name_crawle[i].text)
+    
+    return specific_time, earning_company_name
+
+crawle_earning_data()
+before_total_crawle = {key: value for key, value in zip(earning_company_name,specific_time)}
+
+move_after_page = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[5]/div/div/div/div/div[2]/a").click()
+
+crawle_earning_data()
+after_total_crawle = {key: value for key, value in zip(earning_company_name,specific_time)}
+
+print("장 전 발표:", before_total_crawle)
+print("장 중 발표:", after_total_crawle)
         
 driver.close()
